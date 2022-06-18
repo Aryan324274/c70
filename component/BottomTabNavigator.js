c38da375f2034bc808b8846d30b6e95404dc5ef0
Aryan324274 <dahikararyan8@gmail.com>
2022-06-18 17:24:55 +0530

    c70

--- component/BottomTabNavigator.js
+++ component/BottomTabNavigator.js
@@ -1,20 +1,69 @@
 import React, { Component } from "react";
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import TransactionScreen from "../screens/transcition";
-import SearchScreen from "../screens/search";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-//const Tab = createBottomTabNavigator();
-const Tab=createMaterialBottomTabNavigator()
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+const Tab = createBottomTabNavigator();
 
 export default class BottomTabNavigator extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Transaction") {
+                iconName = "book";
+                size=35
+              } else if (route.name === "Search") {
+                iconName = "search";
+                size=35
+              }
+
+              // You can return any component that you like here!
+              return (
+                <Ionicons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                  
+                />
+              );
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor: "#FFFFFF",
+            inactiveTintColor: "black",
+            style: {
+              height: 130,
+              borderTopWidth: 0,
+              backgroundColor: "#5653d4"
+            },
+            labelStyle: {
+              fontSize: 20,
+              fontFamily: "Rajdhani_600SemiBold"
+            },
+            labelPosition: "beside-icon",
+            tabStyle: {
+              marginTop: 25,
+              marginLeft: 10,
+              marginRight: 10,
+              borderRadius: 30,
+              borderWidth: 2,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#5653d4"
+            }
+          }}
+        >
           <Tab.Screen name="Transaction" component={TransactionScreen} />
-          <Tab.Screen name="Search" component={SearchScreen}/>
+          <Tab.Screen name="Search" component={SearchScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
